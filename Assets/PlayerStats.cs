c6ebdb8b2574f38fd8dca6ed7dc53e7f75e838fe
zrule2222 <domantas.malukas@gmail.com>
2021-03-06 17:51:20 +0200

    Lives

---
+++ Assets/PlayerStats.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public static int lives;
+    public int startLives;
+
+    private void Start()
+    {
+        lives = startLives;
+    }
+}
