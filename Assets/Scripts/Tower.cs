6a302051ded67bcb4008a8f9efac1d0d03dbe7e8
martitis <m.siksnius@ktu.edu>
2021-03-11 15:44:33 +0200

    Tower enemy lock

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -4,22 +4,46 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
-    public Transform target;
-    public float range = 15f;
+    private Transform target;
+    public float range = 2f;
+    public string enemyTag = "Enemy";
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating ("UpdateTarget", 0f, 0.5f);
+    }
+
+    void UpdateTarget() {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if(distanceToEnemy < shortestDistance ) {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if(nearestEnemy != null && shortestDistance <= range) {
+            target = nearestEnemy.transform;
+            Debug.Log("Nusitaike:");
+        } else {
+            target = null;
+            Debug.Log("Nera");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(target == null)
+        return;
     }
 
-    void DrawRangeOnSelected() {
+    void OnDrawGizmosSelected() {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, range);
     }
