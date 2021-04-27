ffab4d28a7d196c174b7bc8cbc05d315cb87f209
Etumot <toma.butenaite@gmail.com>
2021-04-27 13:55:39 +0300

    Background music

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     private bool GameEnded = false;
-
+    public AudioSource backgroundmusic;
     // Update is called once per frame
+    void Start()
+    {
+        backgroundmusic = GetComponent<AudioSource>();
+
+    }
     void Update()
     {
         if (GameEnded)
         {
+            backgroundmusic.Play();
             return;
         }
         if (PlayerStats.lives <= 0)
         {
+            backgroundmusic.Stop();
             EndGame();
         }
     }
