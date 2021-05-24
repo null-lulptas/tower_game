d9496c353d4c63b74f5f5b158c770792870466af
Etumot <toma.butenaite@gmail.com>
2021-05-24 14:30:00 +0300

    Pause menu and volume setting

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,17 +6,19 @@ public class GameManager : MonoBehaviour
 {
     private bool GameEnded = false;
     public AudioSource backgroundmusic;
+    private float musicVolume = 1f;
     // Update is called once per frame
     void Start()
     {
         backgroundmusic = GetComponent<AudioSource>();
+        backgroundmusic.Play();
 
     }
     void Update()
     {
         if (GameEnded)
         {
-            backgroundmusic.Play();
+            backgroundmusic.Play();           
             return;
         }
         if (PlayerStats.lives <= 0)
@@ -24,10 +26,15 @@ public class GameManager : MonoBehaviour
             backgroundmusic.Stop();
             EndGame();
         }
+        backgroundmusic.volume = musicVolume;
     }
     void EndGame()
     {
         GameEnded = true;
         Debug.Log("Game Over");
+    }
+    public void updateVolume(float volume)
+    {
+        musicVolume = volume;
     }
 }
